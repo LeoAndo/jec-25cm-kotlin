d9aa5d3d12a53c49f17481ebcdeb1bc6e58a79f8
LeoAndo <leo.777.info@gmail.com>
2026-09-23 16:15:04 +0900

    first commit

---
+++ HelloKotlin/src/ex05/main.kt
@@ -0,0 +1,17 @@
+package ex05
+
+fun main() {
+    val flag = true
+    if (flag) {
+        println("flag is true")
+    } else {
+        println("flag is false")
+    }
+
+    val yamada = "yamada"
+    if ("Tanaka" == yamada) {
+        println("string is Tanaka")
+    } else {
+        println("string is not Tanaka")
+    }
+}
